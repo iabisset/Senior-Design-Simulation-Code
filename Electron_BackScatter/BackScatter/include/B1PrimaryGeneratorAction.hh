ac42b2038a073f15d74e6192998e3a4c8201a6ee
Iain A Bisset <iain@arch.localdomain>
2020-05-03 00:35:33 -0600

    minor changes

--- Electron_BackScatter/BackScatter/include/B1PrimaryGeneratorAction.hh
+++ Electron_BackScatter/BackScatter/include/B1PrimaryGeneratorAction.hh
@@ -41,9 +41,6 @@ class G4Event;
 class G4Box;
 
 /// The primary generator action class with particle gun.
-///
-/// The default kinematic is a 6 MeV gamma, randomly distribued
-/// in front of the phantom across 80% of the (X,Y) phantom size.
 
 class B1PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 {
